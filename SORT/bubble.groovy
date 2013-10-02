838bd8106e92825f4f7571e2bf655799073aa00d
Irene <irene@aurametrix.com>
2013-10-01 20:16:11 -0700

    more elegant bubble

--- SORT/bubble.groovy
+++ SORT/bubble.groovy
@@ -1,17 +1,16 @@
-class BubbleSort {
-  def list
-  def sort() {
-    def eachPairIndex = { Collection l ->
-      def range = 0..<l.size()
-      [range, range.tail()].transpose()
-    }
-    while (! eachPairIndex(this.list).inject(true) { rslt, idx ->
-      if (! (this.list[idx[0]] < this.list[idx[1]]) ) {
-        use(Collections){ this.list.swap(idx[0], idx[1]) }
-        rslt &= false
-      } else rslt
-    } );
-  }
+def makeSwap = { a, i, j = i+1 -> print "."; a[[i,j]] = a[[j,i]] }
+ 
+def checkSwap = { a, i, j = i+1 -> [(a[i] > a[j])].find { it }.each { makeSwap(a, i, j) } }
+ 
+def BubbleSort = { list ->
+    boolean swapped = true
+    while (swapped) { swapped = (1..<list.size()).any { checkSwap(list, it-1) } }
+    list
 }
 
+
 println (BubbleSort([23,76,-99,5.8,97,37,85,99,11,38,95,92,24,46,41,24,14,12,57,78,4]))
+
+ 
+
+
